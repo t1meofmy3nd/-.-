e1f0455834ca0bf57cd1ed6078edff14a8b3c191
t1meofmy3nd <161883340+t1meofmy3nd@users.noreply.github.com>
2024-08-13 20:42:16 +0400

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 language = "Python"
 number_of_tasks = 12
 time = 1.5
-middle_time = 0.125
+middle_time = time / number_of_tasks
 print("Курс:", language, ", всего задач:", number_of_tasks, ", затрачено часов:", time,", среднее время выполнения", middle_time, "часа.")
